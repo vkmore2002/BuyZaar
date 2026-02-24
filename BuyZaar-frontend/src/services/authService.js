0cfbad6e0a34850adfb105dcfca7554298ce766a
vkmore2002 <vivekmore153@gmail.com>
2026-02-24 14:40:17 +0530

    backend render url added to frontend

--- BuyZaar-frontend/src/services/authService.js
+++ BuyZaar-frontend/src/services/authService.js
@@ -1,4 +1,6 @@
-const BASE_URL = "http://localhost:5000/api/auth";
+const BASE_URL =
+  `${import.meta.env.VITE_API_URL}/api/auth` ||
+  "http://localhost:5000/api/auth";
 
 export const registerUser = async (formData) => {
   const response = await fetch(`${BASE_URL}/register`, {
